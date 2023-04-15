1ffdcdffbec9fd209b72c0c96c14503cc1a4bf35
francksefu <90141102+francksefu@users.noreply.github.com>
2023-04-15 19:04:25 +0200

    Add components and fix linters error for setup react and added initial components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter, Routes, Route, Link,
+} from 'react-router-dom';
 import './App.css';
+import BookList from './components/BookList';
+import BookForm from './components/BookForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+      <BrowserRouter>
+        <h1>Awesome books</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Add of books | </Link>
+            </li>
+            <li>
+              <Link to="/categories"> Categories  </Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<BookForm />} />
+          <Route path="/categories" element={<BookList />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+
   );
 }
 
