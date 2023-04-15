ce47a926b743467575254fbad737c1fb3f5c9e45
francksefu <90141102+francksefu@users.noreply.github.com>
2023-04-15 23:45:46 +0200

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
 import {
-  BrowserRouter, Routes, Route, Link,
+  BrowserRouter, Routes, Route,
 } from 'react-router-dom';
 import './App.css';
 
 import BookForm from './components/BookForm';
 import Categories from './components/Categories';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
 
       <BrowserRouter>
-        <h1>Awesome books</h1>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Add of books | </Link>
-            </li>
-            <li>
-              <Link to="/categories"> Categories  </Link>
-            </li>
-          </ul>
-        </nav>
+        <Navbar />
         <Routes>
           <Route path="/" element={<BookForm />} />
           <Route path="/categories" element={<Categories />} />
